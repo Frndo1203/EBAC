a5e85245247d3503c48ff44c49660ff7b8a682d5
Frndo1203 <oliveirafer06@gmail.com>
2022-03-21 12:42:42 -0300

    Correcting bad handler in lambda function

--- infrastructure/lambda.tf
+++ infrastructure/lambda.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_function" "execute_emr" {
   filename      = "lambda_function_payload.zip"
   function_name = var.lambda_function_name
   role          = aws_iam_role.lambda.arn
-  handler       = "lambda_function_handler"
+  handler       = "lambda_function.handler"
   memory_size   = 128 #mb
   timeout       = 30  #seconds
 
